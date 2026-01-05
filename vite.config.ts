71d1e5aef306cb040e49772d25a2f5dea20c2b8b
Adogo <gonda.ado@gmail.com>
2026-01-05 11:54:57 +0100

    feat: github pages deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/fluid-snake/",
   server: {
     watch: {
       usePolling: true, // Enable polling for file changes
